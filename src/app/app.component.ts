3bcec2ca93277ba5e2b11fb3101b32f3a232e506
Lukasz Marciniak <lmarnavig@gmail.com>
2019-06-20 19:03:19 +0200

    register new modules + change structer of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,55 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'clickmeeting-rec-app';
-  form: FormGroup;
-  figures = [] as string[];
-  calcType = [] as CalcType[];
-  showCalcType = false;
-  showCalc = false;
 
-  constructor(
-    private dataService: DataService,
-    private fb: FormBuilder,
-    private router: Router
-  ) {}
-
-  ngOnInit() {
-    this.router.navigate(['']); // reset route after browser refresh
-    this.fetchData(); // fetch initial data
-
-    // build form with 'two' fields
-    this.form = this.fb.group({
-      figure: ['', Validators.required],
-      calcType: ['', Validators.required]
-    });
-
-    this.onFormChanges(); // subscribe to form changes
-  }
-
-  fetchData() {
-    this.dataService.getFigures().subscribe(figures => {
-      this.figures = figures;
-    });
-  }
-
-  onFormChanges() {
-    this.form.valueChanges.subscribe(form => {
-      const { figure, calcType } = form;
-
-      // fetch data for specified figure to get its own calculation type + show component
-      if (figure) {
-        this.dataService.getCalcTypesForFigure(figure).subscribe(calc => {
-          this.calcType = calc;
-          this.showCalcType = true;
-        });
-      }
-
-      // when user choose two necessary field we can proceed to fetch expresion and calculate it
-      if (figure && calcType) {
-        this.router.navigate([figure, calcType]);
-      }
-    });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalcComponent } from './components/calc/calc.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { PickFigureComponent } from './components/pick-figure/pick-figure.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { CreateFigureComponent } from './components/create-figure/create-figure.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { HttpClientModule } from '@angular/common/http';
+import { ENVIRONMENT_TOKEN } from './core/environment';
+import { environment } from '../environments/environment';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CalcComponent,
+    LayoutComponent,
+    PickFigureComponent,
+    HeaderComponent,
+    NavComponent,
+    CreateFigureComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    NgSelectModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: ENVIRONMENT_TOKEN,
+      useValue: environment
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
