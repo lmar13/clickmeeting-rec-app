180baac98fd027502d603343802587171cbde1da
Lukasz Marciniak <lmarnavig@gmail.com>
2019-06-20 19:03:37 +0200

    add create figure component

---
+++ src/app/components/create-figure/create-figure.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateFigureComponent } from './create-figure.component';
+
+describe('CreateFigureComponent', () => {
+  let component: CreateFigureComponent;
+  let fixture: ComponentFixture<CreateFigureComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CreateFigureComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateFigureComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/create-figure/create-figure.component.ts
@@ -0,0 +1,52 @@
+import { DataService } from 'src/app/shared/data.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-figure',
+  templateUrl: './create-figure.component.html',
+  styleUrls: ['./create-figure.component.scss']
+})
+export class CreateFigureComponent implements OnInit {
+  form: FormGroup;
+  figures: {
+    name: string;
+    tag: boolean;
+  }[];
+
+  constructor(private dataService: DataService, private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.dataService.getFigures().subscribe(figures => this.figures = figures.map(f => ({
+      name: f,
+      tag: false,
+    })));
+    this.form = this.fb.group({
+      fName: ['', Validators.required],
+      cType: ['', Validators.required],
+      exp: ['', Validators.required],
+    });
+  }
+
+  addTag(name) {
+    return { name, tag: true };
+  }
+
+  addNew(value) {
+    this.dataService.addFigure(value).subscribe(res => console.log(res));
+  }
+
+  update(value) {
+    this.dataService.updateFigure(value).subscribe(res => console.log(res));
+  }
+
+  submit() {
+    const { fName, cType, exp } = this.form.value;
+
+    // const newExp = exp.split('').filter(v => v !== ' ').join(' ');
+    // console.log(newExp);
+
+    fName.tag ? this.addNew({ fName, cType, exp }) : this.update({ fName, cType, exp });
+  }
+
+}
