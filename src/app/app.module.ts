e91f94548c8a4e6cc6f722a14023cdff4a9b9cc0
Lukasz Marciniak <lmarnavig@gmail.com>
2019-06-22 22:28:51 +0200

    clean imports and add index.ts in components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CalcComponent } from './components/calc/calc.component';
-import { LayoutComponent } from './components/layout/layout.component';
-import { PickFigureComponent } from './components/pick-figure/pick-figure.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NavComponent } from './components/nav/nav.component';
-import { CreateFigureComponent } from './components/create-figure/create-figure.component';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { HttpClientModule } from '@angular/common/http';
-import { ENVIRONMENT_TOKEN } from './core/environment';
 import { environment } from '../environments/environment';
-
-
+import { AppRoutingModule, components } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ENVIRONMENT_TOKEN } from './core/environment';
+import { LayoutComponent, HeaderComponent, NavComponent } from './components';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalcComponent,
     LayoutComponent,
-    PickFigureComponent,
     HeaderComponent,
     NavComponent,
-    CreateFigureComponent,
+    ...components
   ],
   imports: [
     BrowserModule,
